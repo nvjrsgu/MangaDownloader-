e94f692914e26a5a0cf54c1675d20ad5cd5c8abb
cjvnjde <cjvnjde@ya.ru>
2016-07-25 10:31:36 +0300

    небольшие правки

--- src/LinkCutter.java
+++ src/LinkCutter.java
@@ -29,4 +29,31 @@ public class LinkCutter {
         }
         return url;
     }
+    public String toOneString1(String ... someURL) {
+        String url="";
+        boolean key = false;
+        for(int i = 0; i < someURL.length; i++){
+            if(someURL[i].contains("vol")||key) {
+                key = true;
+                url = url + someURL[i-1]+"/";
+            }
+        }
+        url += someURL[someURL.length-1]+"/";
+        System.out.println("toOneString: "+url);
+        return url;
+    }
+
+    public String toOneString(String ... someURL) {
+        String url="";
+        boolean key = false;
+        for(int i = 0; i < someURL.length; i++){
+            if(someURL[i].contains("vol")||key) {
+                key = true;
+                url = url + someURL[i]+"\\";
+            }
+        }
+        System.out.println("toOneString: "+url);
+        return url;
+    }
+
 }
