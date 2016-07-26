bf00770f7d8a600b924051489c815dac57e11036
cjvnjde <cjvnjde@ya.ru>
2016-07-26 13:34:54 +0300

    улучшен делитель ссылок

--- src/LinkCutter.java
+++ src/LinkCutter.java
@@ -1,26 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 /**
  * Created by cjvnjde on 24.07.2016.
  */
 public class LinkCutter {
-
-    public String[] cutLink(String link) {
-
-        //String
-        String linkArr[];
-        //Убираем ненужные символы
-        if (link.contains("http://")) {
-            link = link.replaceFirst("http://", "");
-        }
-        if (link.contains("\n")) {
-            link = link.replaceAll("\n", "");
+    //делим ссылку по "/"
+    //форматы ссылок:   http://site/page
+    //                  site/page
+    public String[] cutLink(String url) {
+        try {
+            System.setErr(new PrintStream(new File("log.txt")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
-        linkArr = link.split("/");
-        for(String str: linkArr)
-            System.out.print(str+"_");
-        System.out.println();
-        return linkArr;
+        String splittedUrl[];
+
+        String cleanedUrl = cleanURL(url);
+
+        splittedUrl = cleanedUrl.split("/");
+
+        //----------------------------------------
+        System.err.println("LinkCutter - cutLink");
+        for(String str: splittedUrl) {
+            System.err.println("*_"+str+"_*");
+        }
+        //-----------------------------------------
+
+        return splittedUrl;
     }
+
 
     public String toOneURL(String ... someURL) {
         String url="http://";
@@ -29,6 +40,7 @@ public class LinkCutter {
         }
         return url;
     }
+
     public String toOneString1(String ... someURL) {
         String url="";
         boolean key = false;
@@ -55,6 +67,7 @@ public class LinkCutter {
         System.out.println("toOneString: "+url);
         return url;
     }
+
     public String takeMangaName(String url){
         String[] urlArr;
         urlArr = url.split("/");
@@ -81,4 +94,19 @@ public class LinkCutter {
         return linkArr[0];
     }
 
+    
+    //очистка мусора из ссылки
+    private String cleanURL(String url) {
+        //Убираем ненужные символы
+        if (url.contains("http://")) {
+            url = url.replaceFirst("http://", "");
+        }
+        if (url.contains("\n")) {
+            url = url.replaceAll("\n", "");
+        }
+        if (url.contains(" ")){
+            url = url.replaceAll(" ", "");
+        }
+        return url;
+    }
 }
